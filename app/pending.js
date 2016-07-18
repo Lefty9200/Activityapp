6f668c93bffd016fa01f88567f202e2acba2df13
Oliver Isenrich <IsenrichO@gmail.com>
2016-07-18 15:14:25 -0700

    Styling animations, plus dynamical fetching of DB activity data

--- app/pending.js
+++ app/pending.js
@@ -44,28 +44,22 @@ angular.module("pendingModule", [])
   ];
 
 
-  $scope.activity = Database.getUnseenActivities(function(data) {
-    // $scope.activities = data;
-    return data;
+  $scope.joinedEvents = [];
+  $scope.get = Database.getUnseenActivities(function(data) {
+    let result = [];
+
+    for (let active in data) {
+      result.push(data[active]);
+    }
+
+    $scope.joinedEvents = result;
+    $scope.$digest();
+      console.log($scope.joinedEvents);
   });
 
 
   $scope.nextEvent = function(data) {
       console.log(data);
-
-  $scope.SS = [];
-  $scope.activity = Database.getUnseenActivities(function(data) {
-    console.log(data);
-    $scope.SS.push(data);
-  });
-
-  console.log($scope.SS);
-
-
-
-
-
-  $scope.nextEvent = function(data) {
     return $scope.evt = {
       date: data.date,
       time: data.time,
@@ -80,7 +74,6 @@ angular.module("pendingModule", [])
       console.log(data);
 
     joined.push($scope.nextEvent(data));
-    // $scope.nextEvent();
   };
 
 
@@ -89,5 +82,8 @@ angular.module("pendingModule", [])
 
     $scope.nextEvent(data.shift());
   };
+
+
+  
 
 });
